aef5aabdb769f5313a176db94db4f406c837794a
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-02-20 18:02:07 +0000

    Created html_string method - passing the test

---
+++ src/note-list-view.js
@@ -0,0 +1,23 @@
+(function(exports) {
+
+function NoteListView(notelist){
+  this.notelist = notelist;
+};
+
+NoteListView.prototype.html_string = function(notelist){
+
+  var array = notelist.return_array_items();
+  var new_array = [];
+  for (var i in array) {
+    new_array.push(`<li><div>${array[i].text}</div></li>`);
+  }
+  new_array.unshift("<ul>");
+  new_array.push("</ul>");
+  return new_array.join("");
+};
+
+
+
+exports.NoteListView = NoteListView;
+
+})(this);
