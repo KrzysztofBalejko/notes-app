5d421ff69c8a032f7aaff005c1c78d67baf9676c
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-02-21 12:12:19 +0000

    Created NoteController and spiked a new method insert_html

--- note-controller.js
+++ note-controller.js
@@ -1,11 +1,19 @@
 (function(exports){
 
-function NoteController(){
-  
+function NoteController(notelist = new NoteList(), note){
+  this.notelist = notelist.create_note(note)
+  this.view = new NoteListView(notelist)
+};
+
+NoteController.prototype.insert_html = function(view, notelist){
+
+  var id = document.getElementById('app');
+  id.innerHTML = view.html_string(notelist);
+
 };
 
 
-
+exports.NoteController = NoteController;
 
 })(this);
 
