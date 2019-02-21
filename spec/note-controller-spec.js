e5c9fbbe4da157001416e09ffd157c9f47b05ef6
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-02-21 12:10:24 +0000

    Test: NoteController functionality

--- spec/note-controller-spec.js
+++ spec/note-controller-spec.js
@@ -2,3 +2,18 @@ function testNoteControllerCanBeInstantiated(){
   var controller = new NoteController();
   assert.isTrue(controller = new NoteController())
 };
+testNoteControllerCanBeInstantiated()
+
+function testNoteControllerFunctionality(){
+  var notelist = new NoteList();
+  var note = new Note('Favourite drink: seltzer');
+  var controller = new NoteController(notelist, note);
+  var view = new NoteListView(notelist);
+  assert.isTrue(view.html_string(notelist) == "<ul><li><div>" + "Favourite drink: seltzer" + "</div></li></ul>" );
+};
+testNoteControllerFunctionality();
+
+function testMethodGetsHtmlFromNoteListViewAndInsertsItIntoAppElement(){
+
+  
+};
