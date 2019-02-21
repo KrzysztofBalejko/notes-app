2128049ebc186d1a86fe25c46ce313a7b8eba87e
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-02-21 19:01:46 +0000

    Test: html list - work in progress

--- spec/note-controller-spec.js
+++ spec/note-controller-spec.js
@@ -13,7 +13,37 @@ function testNoteControllerFunctionality(){
 };
 testNoteControllerFunctionality();
 
-function testMethodGetsHtmlFromNoteListViewAndInsertsItIntoAppElement(){
 
-  
-};
+function TestHTMLlist(){
+
+  var notelist = new NoteList();
+  var note = new Note('Favourite drink: seltzer');
+  var controller = new NoteController(notelist, note);
+  var view = new NoteListView(notelist);
+
+  controller.create_html = function(){
+   var doc = document.createElement('section')
+   console.log(doc);
+   doc.innerHTML = "<div id='app'>hello</div>"
+   // console.log(doc.innerHTML);
+   return doc
+  }
+
+  console.log(note);
+  console.log(notelist);
+  console.log(controller);
+  console.log(view);
+
+
+  // assert.isTrue(controller.insert_html(view, notelist) === "<ul><div><li>Favourite drink: seltzer</li></div></ul>")
+
+ }
+
+ TestHTMLlist();
+
+//
+//
+// function testMethodGetsHtmlFromNoteListViewAndInsertsItIntoAppElement(){
+//
+//
+// };
