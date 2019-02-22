f3166ff3c4d716afe548ca9118905a88e79524be
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-02-22 12:16:43 +0000

    Minor changes to the test

--- spec/single-note-view-spec.js
+++ spec/single-note-view-spec.js
@@ -1,7 +1,7 @@
 function testMethodReturnsHtmlStringOfNote(){
   var note = new Note('Favourite drink: seltzer');
   var single_note = new SingleNoteView(note);
-  assert.isTrue( single_note.note_model() == "<div>Favourite drink: seltzer</div>" );
+  assert.isTrue(single_note.note_model() == "<div>Favourite drink: seltzer</div>" );
 };
 
 testMethodReturnsHtmlStringOfNote()
